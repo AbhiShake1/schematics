61ba7a28b4f5983f3f8ea8f4aa75514ae1924b00
abhishake1 <abhi9848038283@gmail.com>
2023-11-04 17:07:40 +0000

    fix: imports

--- dist/lib/resource/files/ts/__name__.service.ts
+++ dist/lib/resource/files/ts/__name__.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';<% if (crud && type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>
 import { Create<%= singular(classify(name)) %>Dto } from './dto/create-<%= singular(name) %>.dto';
 import { <%= singular(classify(name)) %>Entity } from './entities/<%= singular(name) %>.entity';
+import { CRUDService } from 'common/service/crud.service';
 import { Update<%= singular(classify(name)) %>Dto } from './dto/update-<%= singular(name) %>.dto';<% } else if (crud) { %>
 import { Create<%= singular(classify(name)) %>Input } from './dto/create-<%= singular(name) %>.input';
 import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';<% } %>
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class <%= classify(name) %>Service extends CRUDService<<%= singular(classify(name)) %>Entity, <%= singular(classify(name)) %>Dto> {
+export class <%= classify(name) %>Service extends CRUDService<<%= singular(classify(name)) %>Entity, Create<%= singular(classify(name)) %>Dto> {
   constructor(
     @InjectRepository(<%= singular(classify(name)) %>Entity)
     readonly <%= lowercased(name) %>Repository: Repository<<%= singular(classify(name)) %>Entity>,
--- src/lib/service/files/ts/__name__.service.ts
+++ src/lib/service/files/ts/__name__.service.ts
@@ -6,9 +6,10 @@ import { Update<%= singular(classify(name)) %>Dto } from './dto/update-<%= singu
 import { Create<%= singular(classify(name)) %>Input } from './dto/create-<%= singular(name) %>.input';
 import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';<% } %>
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class <%= classify(name) %>Service extends CRUDService<<%= singular(classify(name)) %>Entity, <%= singular(classify(name)) %>Dto> {
+export class <%= classify(name) %>Service extends CRUDService<<%= singular(classify(name)) %>Entity, Create<%= singular(classify(name)) %>Dto> {
   constructor(
     @InjectRepository(<%= singular(classify(name)) %>Entity)
     readonly <%= lowercased(name) %>Repository: Repository<<%= singular(classify(name)) %>Entity>,
