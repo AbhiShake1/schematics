ab2d527e8093eb650dd4ae131f0536963798cd09
abhishake1 <abhi9848038283@gmail.com>
2023-11-04 16:34:50 +0000

    fix: entity names

--- dist/lib/resource/files/ts/__name__.service.ts
+++ dist/lib/resource/files/ts/__name__.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';<% if (crud && type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>
 import { Create<%= singular(classify(name)) %>Dto } from './dto/create-<%= singular(name) %>.dto';
 import { CRUDService } from 'common/service/crud.service';
+import { Update<%= singular(classify(name)) %>Dto } from './dto/update-<%= singular(name) %>.dto';<% } else if (crud) { %>
 import { Create<%= singular(classify(name)) %>Input } from './dto/create-<%= singular(name) %>.input';
-import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';<% } %>
 
 @Injectable()
-export class <%= classify(name) %>Service extends CRUDService<<%= classify(name) %>Entity, Create<%= classify(name) %>Dto> {
+export class <%= classify(name) %>Service extends CRUDService<<%= singular(classify(name)) %>Entity, <%= singular(classify(name)) %>Dto> {
   constructor(
-    @InjectRepository(<%= classify(name) %>Entity)
-    readonly <%= lowercased(name) %>Repository: Repository<<%= classify(name) %>Entity>,
+    @InjectRepository(<%= singular(classify(name)) %>Entity)
+    readonly <%= lowercased(name) %>Repository: Repository<<%= singular(classify(name)) %>Entity>,
   ) {
     super(<%= lowercased(name) %>Repository);
   }
