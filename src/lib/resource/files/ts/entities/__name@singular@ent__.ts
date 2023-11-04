100f4a126251fa9d22190af3c9d967c3094941ed
abhishake1 <abhi9848038283@gmail.com>
2023-11-04 17:12:21 +0000

    fix: type constraints

--- src/lib/resource/files/ts/entities/__name@singular@ent__.ts
+++ src/lib/resource/files/ts/entities/__name@singular@ent__.ts
@@ -1,7 +1,8 @@
 <% if (type === 'graphql-code-first') { %>import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { BaseClassEntity } from 'common/entities/base.entity';
 
 @ObjectType()
-export class <%= singular(classify(name)) %>Entity {
+export class <%= singular(classify(name)) %>Entity extends BaseClassEntity {
   @Field(() => Int, { description: 'Example field (placeholder)' })
   exampleField: number;
 }<% } else { %>export class <%= singular(classify(name)) %>Entity {}<% } %>
