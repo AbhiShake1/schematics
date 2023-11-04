c9470a55c1bea19bee58788d0adb43d174ec3966
abhishake1 <abhi9848038283@gmail.com>
2023-11-04 16:51:50 +0000

    fix: unexpected identifier entity

--- src/lib/resource/files/ts/__name__.service.ts
+++ src/lib/resource/files/ts/__name__.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';<% if (crud && type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>
 import { Create<%= singular(classify(name)) %>Dto } from './dto/create-<%= singular(name) %>.dto';
-import { <%= singular(classify(name))Entity %> } from './entities/<%= singular(name) %>.entity';
+import { <%= singular(classify(name)) %>Entity } from './entities/<%= singular(name) %>.entity';
 import { Update<%= singular(classify(name)) %>Dto } from './dto/update-<%= singular(name) %>.dto';<% } else if (crud) { %>
 import { Create<%= singular(classify(name)) %>Input } from './dto/create-<%= singular(name) %>.input';
 import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';<% } %>
 import { InjectRepository } from '@nestjs/typeorm';
-import {  }
 
 @Injectable()
 export class <%= classify(name) %>Service extends CRUDService<<%= singular(classify(name)) %>Entity, <%= singular(classify(name)) %>Dto> {
